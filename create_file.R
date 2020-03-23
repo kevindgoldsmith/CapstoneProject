e4384af2a09694fd2c2156b98c47e6b8cb09ae33
Kevin Goldsmith <kevindgoldsmith@gmail.com>
2020-03-23 21:20:18 +0000

    Updated file create

--- create_file.R
+++ create_file.R
@@ -1,16 +1,15 @@
 create_file <- function(file_name, URL, records = 1000,  proportion = "N") {
   
   assign(deparse(substitute(file_name)), rep(0, records), envir = .GlobalEnv)
-  helper <- rep(0, records)
-  n_lines <- count_lines(URL)
+  
+  lines <- readr::read_lines(URL)
+  n_lines <- length(lines)
   
   if (proportion == "N"){index <- createDataPartition(1:n_lines, p = records/n_lines)}
   if (proportion != "N"){index <- createDataPartition(1:n_lines, p = records)}
-  index <- unlist(index)
-  items <- length(index)
-    for (i in 1:items){
-      helper[i] <- readr::read_lines(URL, skip = index[[i]],n_max = 1)
-    }
   
-  assign(deparse(substitute(file_name)), helper, envir = .GlobalEnv)
+  index <- unlist(index)
+  lines <- lines[index]
+
+  assign(deparse(substitute(file_name)), lines, envir = .GlobalEnv)
 }
