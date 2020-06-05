1462051c756c4a68a8e29c4503accd197c03c0f7
Kevin Goldsmith <kevindgoldsmith@gmail.com>
2020-06-05 18:35:23 +0000

    final!

--- model_build.R
+++ model_build.R
@@ -1,8 +1,44 @@
-model_build <- function(model_prep, n = 3){
-  input <- model_prep %>% filter(ngram %in% 1:2) %>% select(features)
-  preds <- map(input$features, 
-                 next_word, probs = model_prep, verbose = "F", n = n)
+model_build <- function(probs, n = 3){
+  start <- Sys.time()
+  last_time <- start
+  one_gram <- probs %>% filter(ngram == 1) %>% top_frac(.75, true_est)
+  two_gram <- probs %>% filter(ngram == 2) %>% top_frac(.75, true_est)
+  input <- rbind(one_gram, two_gram)
+  input %<>% dplyr::select(features)
+  input <- as.vector(input$features)
+  rm(one_gram)
+  rm(two_gram)
+  out <- setNames(data.frame(matrix(ncol = 3, nrow = length(input))), 
+                             c("pred1", "pred2", "pred3"))
+  chunks <- split(input, ceiling(seq_along(input)/(length(input)/20)))
+  row_num = 1
+  numCores <- detectCores()
+  registerDoParallel(numCores)
+  for (i in 1:length(chunks)){
+
+      preds <- mclapply(chunks[[i]],
+                next_word,
+                probs = probs,
+                verbose = "F",
+                n = n,
+                mc.cores = numCores,
+                mc.preschedule = TRUE)
+    preds <- as.data.frame(do.call(rbind, preds), stringsAsFactors = FALSE)
+    names(preds) <- c("pred1", "pred2", "pred3")
+  out[row_num:(row_num + nrow(preds) - 1), ] <- preds
+  row_num <- row_num + nrow(preds) 
+  rm(preds)
+  now_time <- Sys.time()
+  total_time <- difftime(now_time, start, units = "mins")
+  this_round <- difftime(now_time, last_time, units = "mins")
+  last_time <- now_time
+  cat(i, " of ", length(chunks), "completed\n")
+  cat(this_round, "m round time\n")
+  cat(total_time / i, "m average time\n")
+  cat(total_time, "m total time\n\n")
+  }
   
-  results <- cbind("inputs" = input$features, "preds" = preds)
+  stopImplicitCluster()
+  results <- cbind(input, out)
   results
 }
--- next_word.R
+++ next_word.R
@@ -2,59 +2,58 @@ next_word <- function(string, probs, verbose = T, n = 3){
   ### determine number of words
   words <- str_count(string, pattern = "_") + 1
   
-  ### save last word
+  ###save last word and prob 
   w <- word(string, - 1, sep = "_")
-  
-  ### save first word
-  w_n1 <- word(string, -2, sep = "_")
-
-  ### prob of observing last word
   p_w <- probs[which(probs$features == w), "true_est"]
   
-  ### prob of observing both words together 
-  p_w1 <- probs[which(probs$features == paste0(w_n1,"_", w)), 
-    "true_est"]
+  ##if more than 1 word, 3 gram logic 
+  if(words > 1){
+         w_n1 <- word(string, -2, sep = "_")
+         p_w1 <- probs[which(probs$features == paste0(w_n1,"_", w)), "true_est"]
+         possible_words3 <- filter(probs, 
+                                   startsWith(features,
+                                              paste0(w_n1, "_", w,"_")),
+                                   ngram == 3)
+         possible_words3 <- mutate(possible_words3, prob = pmin(gt_est / p_w1, 1))
+         alpha3 <- 1 - sum(possible_words3$prob)
+  }
   
-  ### find all 3-grams starting with string 
-  possible_words3 <- filter(probs, 
-                            startsWith(features,
-                                       paste0(w_n1, "_", w,"_")),
-                            ngram == 3)
-  possible_words3 <- possible_words3 %>% 
-    mutate(last = word(features, -1, sep = "_"))
-  
-  ### final all 2-grams starting with last word 
+  ### 2 gram logic  
   possible_words2 <- filter(probs, 
                             startsWith(features, paste0(w,"_")),
                             ngram == 2)
-  possible_words2 <- possible_words2 %>% 
-    mutate(last = word(features, -1, sep = "_"))
-  possible_words2 <- anti_join(possible_words2, possible_words3,
-                               by = c("last" = "last"))
+  ## antijoin 2 grams and 3 grams if words > 1
+  if(words > 1){
+    possible_words2 <- anti_join(possible_words2, possible_words3,
+                                 by = c("last" = "last"))
+    possible_words2 <- mutate(possible_words2, prob = alpha3 * pmin(gt_est / p_w, 1))
+  }
+  else
+  {possible_words2 <- mutate(possible_words2, prob = pmin(gt_est / p_w, 1))}
   
-  possible_words3 <- mutate(possible_words3, prob = pmin(gt_est / p_w1, 1))
-  alpha3 <- 1 - sum(possible_words3$prob)
   
-  possible_words2 <- mutate(possible_words2, prob = alpha3 * pmin(gt_est / p_w, 1))
   alpha2 <- 1 - sum(possible_words2$prob)
   
-  lowest <- suppressWarnings(
-    min(min(possible_words3$prob), min(possible_words2$prob)))
-  
-  if(lowest == Inf){lowest <- 0}
-  
-  possible_words1 <- filter(probs, 
-                            gt_est > lowest,
-                            ngram == 1)
-  possible_words1 <- possible_words1 %>% 
-    mutate(last = word(features, -1, sep = "_"))
+  possible_words1 <- probs %>% filter(ngram == 1) %>% top_n(10, gt_est)
   possible_words1 <- anti_join(possible_words1, possible_words2,
                                by = c("last" = "last"))
-  possible_words1 <- anti_join(possible_words1, possible_words3,
-                               by = c("last" = "last"))
+  if(words > 1){
+    possible_words1 <- anti_join(possible_words1, possible_words3,
+                                 by = c("last" = "last"))
+  }
   possible_words1 <- mutate(possible_words1, prob = alpha2 * gt_est)
   
-  preds <- rbind(possible_words2, possible_words3, possible_words1)
+  if(words > 1){
+    possible_words3 %<>% top_n(10, prob)
+  }
+  possible_words2 %<>% top_n(10, prob)
+  possible_words2 %<>% top_n(10, prob)
+  if(words > 1){
+    preds <- rbind(possible_words2, possible_words3, possible_words1)
+  }
+  else{
+    preds <- rbind(possible_words2, possible_words1)
+  }
   preds %<>% dplyr::select(c(last, ngram, prob))
   preds <- preds[order(preds$prob, decreasing = TRUE),]
   if(verbose == T) {
